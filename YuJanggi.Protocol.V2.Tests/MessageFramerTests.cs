55a4a221243aa8c794a3a52ff7a064c3c999def7
OilStone <you8343@gmail.com>
2026-09-18 17:48:52 +0900

    test: 프로토콜 왕복 및 프레이밍 예외 테스트 추가
    - Handshake Request/Response 왕복 테스트 추가
    - MessageFramer 빈 본문 예외 테스트 추가
    - 최대 본문 크기 초과 테스트 추가
    - 잘못된 헤더 크기 테스트 추가

---
+++ YuJanggi.Protocol.V2.Tests/MessageFramerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using YuJanggi.Protocol.V2.Connection;
+using YuJanggi.Protocol.V2.Framing;
+using YuJanggi.Protocol.V2.Messages;
+using YuJanggi.Protocol.V2.Messages.MessageFactory;
+using YuJanggi.Protocol.V2.Serialization;
+
+namespace YuJanggi.Protocol.V2.Tests
+{
+    [TestClass]
+    public class MessageFramerTests
+    {
+        [TestMethod]
+        public void MessageFramer_Encode_EmptyBody_ThrowsInvalidDataException()
+        {
+            byte[] body = Array.Empty<byte>();
+
+            Assert.ThrowsExactly<InvalidDataException>(
+                () => MessageFramer.Encode(body));
+        }
+
+        [TestMethod]
+        public void MessageFramer_Encode_TooLargeBody_ThrowsInvalidDataException()
+        {
+            byte[] body = new byte[MessageFramer.MaxBodySize + 1];
+
+            Assert.ThrowsExactly<InvalidDataException>(
+                () => MessageFramer.Encode(body));
+        }
+
+        [TestMethod]
+        public void MessageFramer_DecodeBodyLength_InvalidHeaderSize_ThrowsArgumentException()
+        {
+            byte[] header = new byte[MessageFramer.HeaderSize - 1];
+
+            Assert.ThrowsExactly<ArgumentException>(
+                () => MessageFramer.DecodeBodyLength(header));
+        }
+    }
+}
---
+++ YuJanggi.Protocol.V2.Tests/ProtocolRoundTripTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using YuJanggi.Protocol.V2.Connection;
+using YuJanggi.Protocol.V2.Framing;
+using YuJanggi.Protocol.V2.Messages;
+using YuJanggi.Protocol.V2.Messages.MessageFactory;
+using YuJanggi.Protocol.V2.Serialization;
+
+namespace YuJanggi.Protocol.V2.Tests
+{
+    [TestClass]
+    public class ProtocolRoundTripTests
+    {
+        [TestMethod]
+        public void ProtocolHandshakeRequest_RoundTrip_Success()
+        {
+            var request = new ProtocolHandshakeRequest
+            {
+                YuJanggiProtocolVersion = "0.1.0",
+                YuJanggiCoreVersion = "1.0.0"
+            };
+
+            ClientMessage message =
+                ClientMessageFactory.Create(
+                    ClientMessageType.ProtocolHandshake,
+                    request);
+
+            byte[] body =
+                MessageSerializer.Serialize(message);
+
+            byte[] packet =
+                MessageFramer.Encode(body);
+
+            int bodyLength =
+                MessageFramer.DecodeBodyLength(
+                    packet.AsSpan(0, MessageFramer.HeaderSize));
+
+            ReadOnlySpan<byte> receivedBody =
+                packet.AsSpan(
+                    MessageFramer.HeaderSize,
+                    bodyLength);
+
+            ClientMessage receivedMessage =
+                MessageSerializer.Deserialize<ClientMessage>(
+                    receivedBody);
+
+            ProtocolHandshakeRequest receivedRequest =
+                receivedMessage.GetPayload<ProtocolHandshakeRequest>();
+
+            Assert.AreEqual(
+                ClientMessageType.ProtocolHandshake,
+                receivedMessage.Type);
+
+            Assert.AreEqual(
+                request.YuJanggiProtocolVersion,
+                receivedRequest.YuJanggiProtocolVersion);
+
+            Assert.AreEqual(
+                request.YuJanggiCoreVersion,
+                receivedRequest.YuJanggiCoreVersion);
+
+            Assert.IsFalse(
+                string.IsNullOrWhiteSpace(receivedMessage.RequestId));
+        }
+
+        [TestMethod]
+        public void ProtocolHandshakeResponse_RoundTrip_Success()
+        {
+            string requestId = Guid.NewGuid().ToString();
+
+            var response = new ProtocolHandshakeResponse
+            {
+                Result = ProtocolHandshakeResult.Success
+            };
+
+            ServerMessage message =
+                ServerMessageFactory.CreateResponse(
+                    ServerMessageType.ProtocolHandshake,
+                    requestId,
+                    response);
+
+            byte[] body =
+                MessageSerializer.Serialize(message);
+
+            byte[] packet =
+                MessageFramer.Encode(body);
+
+            int bodyLength =
+                MessageFramer.DecodeBodyLength(
+                    packet.AsSpan(0, MessageFramer.HeaderSize));
+
+            ReadOnlySpan<byte> receivedBody =
+                packet.AsSpan(
+                    MessageFramer.HeaderSize,
+                    bodyLength);
+
+            ServerMessage receivedMessage =
+                MessageSerializer.Deserialize<ServerMessage>(
+                    receivedBody);
+
+            ProtocolHandshakeResponse receivedResponse =
+                receivedMessage.GetPayload<ProtocolHandshakeResponse>();
+
+            Assert.AreEqual(
+                ServerMessageType.ProtocolHandshake,
+                receivedMessage.Type);
+
+            Assert.AreEqual(
+                requestId,
+                receivedMessage.RequestId);
+
+            Assert.AreEqual(
+                ProtocolHandshakeResult.Success,
+                receivedResponse.Result);
+        }
+    }
+}
